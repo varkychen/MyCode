19c36c617f56f4fecca2ee2575385b8d8d9f186a
Varghese Paul <varkychen@gmail.com>
2016-07-29 08:17:31 +0530

    Insertion sort using for loop

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -6,11 +6,15 @@ a = sample(range(101),n)
 print "Sort a sample of", n, "elements in ascending order. Input array:", a
 
 for i in range(1,n):
-    smallest = a[i]
-    j = i;
-    while j > 0 and a[j-1] > smallest:
-        a[j] = a[j-1]
-        j -= 1
-    a[j] = smallest
+    to_insert = a[i]
+    for j in range(i, -1, -1):
+        if j == 0:
+            a[j] = to_insert
+        elif a[j-1] > to_insert:
+            a[j] = a[j-1]
+        else:
+            a[j] = to_insert
+            break
+    print "after loop j:", j
     print "Intermediate array", i, ":", a
 print a
